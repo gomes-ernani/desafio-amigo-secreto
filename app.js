8e5eb13120a3bce910c13c15289f16e08c638e80
Ernani Gomes <36718194+gomes-ernani@users.noreply.github.com>
2025-08-03 15:23:23 -0300

    create function to add friend

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Inicia declarando uma variável do tipo array, que armazenará os nomes dos amigos inseridos
+let amigos = [];
+
+// Função que permite ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente
+function adicionarAmigo() {
+    // Capturar o valor do campo de entrada
+    const campoAmigo = document.getElementById('amigo');
+    const nomeAmigo = campoAmigo.value.trim(); // Usado .trim() para remover espaços em branco no início e no fim
+
+    // Validar a entrada
+    if (nomeAmigo === '') {
+        alert('Por favor, insira um nome.');
+        return; // Encerra a função se o campo estiver vazio
+    }
+
+    // Atualizar o array de amigos
+    amigos.push(nomeAmigo);
+
+    // Chamar a função para atualizar a visualização da lista na tela
+    atualizarLista();
+
+    // Limpar o campo de entrada
+    campoAmigo.value = '';
+    campoAmigo.focus(); // Coloca o cursor de volta no campo para facilitar a adição de novos nomes
+}
